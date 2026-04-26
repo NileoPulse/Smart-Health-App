cf8ceb00849a0671c9bc5f294858cd28e775c04c
mayarabouzaid <myar25629@gmail.com>
2026-04-27 01:25:52 +0200

    update

--- android/app/src/main/kotlin/com/example/smarthealth/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.smarthealth
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,15 +1,11 @@
 pluginManagement {
-    val flutterSdkPath =
-        run {
-            val properties = java.util.Properties()
-            file("local.properties").inputStream().use { properties.load(it) }
-            val flutterSdkPath = properties.getProperty("flutter.sdk")
-            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-            flutterSdkPath
-        }
-
+    val flutterSdkPath = run {
+        // هنا استعملنا المسار الكامل للمكتبة عشان نتفادى إيرور الـ Unresolved reference
+        val properties = java.util.Properties() 
+        file("local.properties").inputStream().use { properties.load(it) }
+        properties.getProperty("flutter.sdk") ?: throw GradleException("flutter.sdk not set in local.properties")
+    }
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
-
     repositories {
         google()
         mavenCentral()
@@ -17,10 +13,11 @@ pluginManagement {
     }
 }
 
+
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.11.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
+    id("com.android.application") version "8.7.3" apply false  // ← من 8.1.0
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false  // ← من 1.8.22
 }
 
 include(":app")
